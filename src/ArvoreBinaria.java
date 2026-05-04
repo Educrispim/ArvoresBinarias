79ff6719eb6f8cd9e41ad8a9bfaceb8a839f0ead
ricardo <ricardo.petri@unifil.br>
2026-05-04 16:32:55 -0300

    primeiro commit

---
+++ src/ArvoreBinaria.java
@@ -0,0 +1,12 @@
+public class ArvoreBinaria {
+    No raiz;
+
+    public ArvoreBinaria() {
+        this.raiz = new No(null);
+        System.out.println("Árvore Binária criada com sucesso!");
+    }
+
+    public void inserir(Integer conteudo) {
+
+    }
+}
