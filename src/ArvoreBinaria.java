288c29352a452c68c8cdac4adbc4cd55f06d803e
ricardo <ricardo.petri@unifil.br>
2026-05-05 20:13:47 -0300

    Implementação árvores binarias

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -7,6 +7,83 @@ public class ArvoreBinaria {
     }
 
     public void inserir(Integer conteudo) {
+        No novoNo = new No(conteudo);
 
+        if(estaVazia()) {
+            this.raiz = novoNo;
+        } else {
+            No aux = this.raiz;
+            while(true) {
+                if (aux.getConteudo() > novoNo.getConteudo()) {
+                    if (aux.getEsquerda() == null) {
+                        aux.setEsquerda(novoNo);
+                        return;
+                    } else {
+                        aux = aux.getEsquerda();
+                    }
+                } else if (aux.getConteudo() == novoNo.getConteudo()) {
+                    System.out.println("Não é possível informar nós repetidos.");
+                    return;
+                } else {
+                    if (aux.getDireita() == null) {
+                        aux.setDireita(novoNo);
+                        return;
+                    } else {
+                        aux = aux.getDireita();
+                    }
+                }
+            }
+        }
     }
+
+    public boolean estaVazia() {
+        if(this.raiz.getConteudo() == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void preOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        System.out.println(no.getConteudo());
+        preOrdem(no.getEsquerda());
+        preOrdem(no.getDireita());
+    }
+
+    private void emOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        emOrdem(no.getEsquerda());
+        System.out.println(no.getConteudo());
+        emOrdem(no.getDireita());
+    }
+
+    private void posOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        posOrdem(no.getEsquerda());
+        posOrdem(no.getDireita());
+        System.out.println(no.getConteudo());
+    }
+
+    public void exibir(String percurso) {
+        switch (percurso){
+            case("Pre"):
+                preOrdem(this.raiz);
+                break;
+            case("Em"):
+                emOrdem(this.raiz);
+                break;
+            case("Pos"):
+                posOrdem(this.raiz);
+                break;
+        }
+    }
+
+
 }
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+        arvoreBinaria.inserir(7);
+        arvoreBinaria.inserir(8);
+        arvoreBinaria.inserir(11);
+        arvoreBinaria.inserir(10);
+        arvoreBinaria.inserir(9);
+        arvoreBinaria.inserir(14);
+        arvoreBinaria.inserir(5);
+        arvoreBinaria.inserir(2);
+        arvoreBinaria.inserir(-10);
+        arvoreBinaria.inserir(3);
+        arvoreBinaria.exibir("Pre");
+
+    }
+}
